79a55787de314201e2f008dbbfa0700cc9ca01a1
katerinamakri <kathrin023@gmail.com>
2022-01-23 17:39:21 +0200

    Working on Table component and its style

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,8 +3,14 @@ import Row from './Row.jsx';
 
 function Table() {
 	return (
-		<div className="flex justify-center">
-			<Row/>
+		<div className="flex justify-center mt-5">
+			<div className="gaming-board">
+				<Row/>
+				<Row/>
+				<Row/>
+				<Row/>
+				<Row/>
+			</div>
 		</div>
 	)
 }
