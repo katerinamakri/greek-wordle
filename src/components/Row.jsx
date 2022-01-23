044f1ba730ca3bc979732fd2197a892c66b04aef
katerinamakri <kathrin023@gmail.com>
2022-01-23 17:38:46 +0200

    Working on Row component and its style

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,23 +1,13 @@
 import '../css/Row.css'
 
 function Row() {
-	return(
-		<ul id="game-board" class="deck">
-			<li class="card">
-				<i class="fa fa-paper-plane-o"></i>
-			</li>
-			<li class="card">
-				<i class="fa fa-anchor"></i>
-			</li>
-			<li class="card">
-				<i class="fa fa-diamond"></i>
-			</li>
-			<li class="card">
-				<i class="fa fa-diamond"></i>
-			</li>
-			<li class="card">
-				<i class="fa fa-diamond"></i>
-			</li>
+	return (
+		<ul class="flex justify-between align-middle mt-5">
+			<li class="card"></li>
+			<li class="card"></li>
+			<li class="card"></li>
+			<li class="card"></li>
+			<li class="card"></li>
 		</ul>
 	);
 }
