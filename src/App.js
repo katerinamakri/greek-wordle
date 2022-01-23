1f123c8d855985a36513ea009e47e710e6152305
katerinamakri <kathrin023@gmail.com>
2022-01-23 16:23:12 +0200

    Working on header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
+import './css/App.css';
 import Table from './components/Table.jsx';
 //import Keyboard from './components/Keyboard.jsx';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Greek Wordle</h1>
-      </header>
-      <Table/>
-      {/*<Keyboard/>*/}
-    </div>
-  );
+	return (
+		<div className="text-center">
+			<header className="border-b">
+				<div className="flex justify-between">
+					<button>Info</button>
+					<h1>Greek Wordle</h1>
+					<button>Stats</button>
+				</div>
+			</header>
+			<Table/>
+			{/*<Keyboard/>*/}
+		</div>
+	);
 }
 
 export default App;
