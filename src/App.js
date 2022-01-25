57f17591fb06938c940158737d713e14aa6ef865
katerinamakri <kathrin023@gmail.com>
2022-01-25 17:06:21 +0200

    Add background image and reorder elements at header

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import Keyboard from './components/Keyboard.jsx';
 
 function App() {
 	return (
-		<div className="text-center">
-			<header className="border-b">
-				<div className="flex justify-between py-2">
-					<button>Info</button>
-					<img src={Logo} alt="logo" width="190"/>
-					<button>Stats</button>
+		<div className="text-center background h-screen">
+			<header>
+				<div className="flex justify-between py-2 px-2">
+					<img src={Logo} alt="logo" width="150"/>
+					<div className="flex justify-between">
+						<button className="mr-3 info-button px-2">
+							<i className="fas fa-info"></i>
+						</button>
+						<button className="chart-button mt-1">
+							<i className="fas fa-chart-line"></i>
+						</button>
+					</div>
 				</div>
 			</header>
-			<Table/>
-			<Keyboard/>
+
+			<main className="container mx-auto">
+				<Table/>
+				<Keyboard/>
+			</main>
+
 			<footer>
 				<p>Copyright 2022</p>
 			</footer>
