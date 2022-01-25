ca05f6a2de2b5376a43dc0ade27123c380449023
katerinamakri <kathrin023@gmail.com>
2022-01-25 17:23:03 +0200

    Align center keyboard

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -5,29 +5,31 @@ import { keys } from '../data/keys.js';
 function Keyboard() {
 
 	return (
-		<div className="keyboard-container mx-auto mb-5 w-24">
-			<div className="first-row flex justify-center w-10">
-				{
-					keys.slice(0, 9).map((item) => (
-						<Key letter={item} key={item}/>
-					))
-				}
-			</div>
+		<div className="flex justify-center mb-5">
+			<div>
+				<div className="first-row flex justify-center w-10">
+					{
+						keys.slice(0, 9).map((item) => (
+							<Key letter={item} key={item}/>
+						))
+					}
+				</div>
 
-			<div className="second-row flex justify-center w-10 my-2">
-				{
-					keys.slice(9, 19).map((item) => (
-						<Key letter={item} key={item}/>
-					))
-				}
-			</div>
+				<div className="second-row flex justify-center w-10 my-2">
+					{
+						keys.slice(9, 19).map((item) => (
+							<Key letter={item} key={item}/>
+						))
+					}
+				</div>
 
-			<div className="third-row flex justify-center w-10">
-				{
-					keys.slice(19, 27).map((item) => (
-						<Key letter={item} key={item}/>
-					))
-				}
+				<div className="third-row flex justify-center w-10">
+					{
+						keys.slice(19, 27).map((item) => (
+							<Key letter={item} key={item}/>
+						))
+					}
+				</div>
 			</div>
 		</div>
 	);
